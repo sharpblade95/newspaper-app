9868602bebe676bbaee1b49b0d987627e128d971
foozhan <f.ataiefard@gmail.com>
2018-09-10 13:57:48 +0430

    user authentication added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import CustomUserCreationForm , CustomUserChangeForm
 # Create your views here.
+
+class SignUp(generic.CreateView):
+	form_class = CustomUserCreationForm
+	succes_url = reverse_lazy('login')
+	template_name = 'signup.html'
